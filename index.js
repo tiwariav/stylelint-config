8117bc02e1da2b4b40fa3b58f0ed5ff12caf0b79
Arvind Tiwari <tiwariav@gmail.com>
2024-01-09 16:25:57 +0530

    fix: add ':global' to selector-pseudo-class-no-unknown

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ module.exports = {
     "font-weight-notation": "numeric",
     "function-no-unknown": null,
     "max-nesting-depth": 4,
+    "selector-pseudo-class-no-unknown": [true, [":global"]],
     "csstools/media-use-custom-media": "always",
     "csstools/use-nesting": "always",
     "csstools/value-no-unknown-custom-properties": true,
